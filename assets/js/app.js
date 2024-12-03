b09a0dff4f253f6b01cd24dbf4233b20c7722092
Rodrigo Vieira <dsrodrigovieira@gmail.com>
2024-12-03 20:10:28 -0300

    iniciada implementacao do cadastro de novos usuarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/auth';
+const API_URL = 'http://localhost:3000';
 
 // // Função para verificar se o token está presente e válido no cookie (PRD)
 // function checkAuth() {
@@ -39,14 +39,14 @@ if (window.location.pathname.includes('home.html')) {
   checkAuth();
 }
 
-document.getElementById('form-btn').addEventListener('click', async (e) => {
+document.getElementById('btn-login').addEventListener('click', async (e) => {
   e.preventDefault();
 
-  const email = document.getElementById('user-email').value;
-  const password = document.getElementById('user-pw').value;
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
 
   try {
-    const response = await fetch(`${API_URL}`, {
+    const response = await fetch(`${API_URL}/auth`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
@@ -67,4 +67,47 @@ document.getElementById('form-btn').addEventListener('click', async (e) => {
   } catch (error) {
     alert('Erro: '+error.message);
   }
+});
+
+document.getElementById('btn-register').addEventListener('click', async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById('new-name').value;
+  const lastname = document.getElementById('new-lastname').value;
+  const username = document.getElementById('new-username').value;
+  const email = document.getElementById('new-email').value;
+  const password = document.getElementById('new-password').value;
+  const password_check = document.getElementById('new-password2').value;
+
+  // CRIANDO LOGICA PARA GERAR USERNAME E INSERT DE NOVO USUARIO
+
+  if (password != password_check) {
+    alert("Senha não confere");
+  } else {
+    try {
+      const response = await fetch(`${API_URL}/register`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ fullname, username, email, password }),
+        credentials: 'include', // Inclui cookies na requisição
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Falha no cadastro. ${response.message}`);
+      }
+  
+      const data = await response.json();
+      
+      alert(data.message);
+  
+      // Redireciona para a página home após login bem-sucedido
+      window.location.href = 'index.html';    
+      //console.log('Token:', data.token);
+    } catch (error) {
+      alert('Erro: '+error.message);
+    }
+
+  }
+
+
 });
