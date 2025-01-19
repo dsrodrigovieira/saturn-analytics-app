d3849e965bdfbc5ca2fe45b39d7718efa500a30c
dsrodrigovieira <dsrodrigovieira@gmail.com>
2025-01-19 18:56:41 -0300

    incluida logica do login

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,20 @@
-document.getElementById('btn-enter').addEventListener('click', async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("https://saturn-api.vercel.app/auth/demo", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        // body: JSON.stringify({ email, password }),
-        // credentials: 'include' // Inclui cookies na requisição
-      });
-      if (!response.ok) {
-        throw new Error(`Falha no login. ${response.message}`);
-      }
-      const data = await response.json();
-      sessionStorage.setItem('organizationCnes', data.cnes);    
-      sessionStorage.setItem('message', data.message);    
-      alert(data.message);
-      // Redireciona para a página home após login bem-sucedido
-      window.location.href = 'home2.html';      
-    } catch (error) {
-      alert('Erro: '+error.message);
+import { LOGIN_DEMO_URL } from './data.js';
+
+document.getElementById('btn-enter').addEventListener('click', async (e) => { 
+  e.preventDefault();
+  try {
+    const response = await fetch(LOGIN_DEMO_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (!response.ok) {
+      throw new Error(`Falha no login. ${response.message}`);
     }
-  });
+    const data = await response.json();
+    sessionStorage.setItem('organizationCnes', data.cnes);    
+    sessionStorage.setItem('message', data.message);
+    window.location.href = 'home.html';      
+  } catch (error) {
+    alert('Erro: '+error.message);
+  }
+});
