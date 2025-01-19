92700d6b93445f4d948716933d6a552c3eb9d0cc
dsrodrigovieira <dsrodrigovieira@gmail.com>
2025-01-19 18:55:10 -0300

    incluidos endpoints da api, operators e variacao

--- assets/js/data.js
+++ assets/js/data.js
@@ -32,3 +32,23 @@ export const filtro_anos = [
         ]
     }
 ];
+export const API_URL = "https://saturn-api.vercel.app";
+export const KPI_RESULTS_URL = API_URL+"/results";
+export const KPI_INFO_URL = API_URL+"/kpi";
+export const LOGOUT_URL = API_URL+"/logout";
+export const LOGIN_DEMO_URL = API_URL+"/auth/demo";
+export const AUTH_VALIDATION_URL = API_URL+"/auth/validade";
+export const operators = {
+    '>=': function (a, b){ return a>=b },
+    '<=': function (a, b){ return a<=b },
+    '>' : function (a, b){ return a>b },
+    '<' : function (a, b){ return a<b }
+};
+export const variacao_c = `<i class="fa-solid fa-angles-up fa-xl"></i>`;
+export const variacao_d = `<i class="fa-solid fa-angles-up fa-rotate-180 fa-xl"></i>`;
+export const variacao_n = `<i class="fa-solid fa-grip-lines fa-xl"></i>`;
+export const variacao = {
+    c: variacao_c,
+    d: variacao_d,
+    n: variacao_n
+};
