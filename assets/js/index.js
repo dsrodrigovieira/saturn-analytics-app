053e9208d43a8952bca97abaac20bae0f28431fe
dsrodrigovieira <dsrodrigovieira@gmail.com>
2025-01-19 22:39:27 -0300

    adicionado spinner de carregamento

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,25 @@
 import { LOGIN_DEMO_URL } from './data.js';
 
-document.getElementById('btn-enter').addEventListener('click', async (e) => { 
+const btn_enter = document.getElementById('btn-enter');
+const toast = document.getElementById("toast-wrapper");
+const content_toast = `
+<div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    <strong class="me-auto">Falha no login</strong>
+    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+  </div>
+  <div class="toast-body bg-warning">
+    Houve uma falha no login. Por favor, tente novamente.
+  </div>
+</div>
+`;
+
+btn_enter.addEventListener('click', async (e) => { 
   e.preventDefault();
+  btn_enter.innerHTML = `
+  <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+  <span class="visually-hidden">Loading...</span>
+  `
   try {
     const response = await fetch(LOGIN_DEMO_URL, {
       method: 'POST',
@@ -16,6 +34,7 @@ document.getElementById('btn-enter').addEventListener('click', async (e) => {
     sessionStorage.setItem('message', data.message);
     window.location.href = 'home.html';      
   } catch (error) {
-    alert('Erro: '+error.message);
+    btn_enter.innerHTML = `Entrar`
+    toast.innerHTML = content_toast;
   }
 });
